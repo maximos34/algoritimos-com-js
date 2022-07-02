c65c9357335ef9c8c5840d0620f1cd8e6f8b7839
pedro falcao <maximosjoselages@gmail.com>
2022-07-01 22:56:17 -0300

    atualização das aulas

---
+++ desafio-setença-e-bloco/conversor.js
@@ -0,0 +1,11 @@
+// let graus = 73 ; 
+// const converger = (graus-32) / 1.8 ;
+
+// console.log('estamos atualmente com '+graus+' graus Fahrenheit e a mesma temperatura em celsius dá '+converger);
+
+
+let F = 73
+
+let c = ((F -32) / 9) *5
+console.log(c)
+
