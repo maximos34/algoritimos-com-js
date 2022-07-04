028554055f1c84554f200326d3fb23c9eab6e13d
pedro falcao <maximosjoselages@gmail.com>
2022-07-04 17:14:03 -0300

    organizando

---
+++ cursodeLogicacomjs/fundamentos/conteudos/tipos de dados/operadoresAritimetico.js
@@ -0,0 +1,30 @@
+/*
+              . Operadores Aritméticos{
+
+            Soma (Soma = + )
+            Subtração (Subtração = + )
+            Divisão (Divisão = + )
+            Mutiplicação (Mutiplicação = + )
+            Modulo (Modulo = + )
+
+
+
+              }
+*/
+
+
+
+const num1 = 85
+const num2 = 62
+
+
+const soma = num1+num2
+console.log(soma)
+const subtração = num1-num2
+console.log(subtração)
+const divisão = num1/num2
+console.log(divisão)
+const mutiplicação = num1*num2
+console.log(mutiplicação)
+const modulo = num1%num2
+console.log(modulo)
