6cfe55e8d58e040b366b6338dbbaa6a71a16ff19
pedro falcao <maximosjoselages@gmail.com>
2022-07-13 09:53:05 -0300

    rs

---
+++ fundamentos/exercicios/desafiosDeAtribuicao/desafio1.js
@@ -0,0 +1,13 @@
+/* Desafio da aula : 
+================== 
+lista de aprovados 
+================== 
+*/ 
+
+let texto = 'lista de aprovados '
+texto += '\n ================== '
+texto += '\n 1. Ana Silva '
+texto += '\n 2. Pedro Albuquerque '
+texto += '\n 3. Guilherme Pereira '
+texto += '\n 4. Rebeca França '
+console.log(texto)
