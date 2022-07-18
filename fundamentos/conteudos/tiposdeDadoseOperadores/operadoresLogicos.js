752542dd443a9e51a15b8c9721afee25beb757a4
pedro falcao <maximosjoselages@gmail.com>
2022-07-18 12:14:17 -0300

    aula de operadores lógicos
    arquivo com alguns operadores lógicos em javaScript

---
+++ fundamentos/conteudos/tiposdeDadoseOperadores/operadoresLogicos.js
@@ -0,0 +1,12 @@
+/*
+              . Operadores Logicos{
+                
+                and (&&)-- "comando logico e"
+                or (||)-- "comando logico ou"
+                not (!) -- "negação logica"
+              }
+              respostas dos operadores logicos {
+                sim (true)
+                falso (false)
+              }
+*/
