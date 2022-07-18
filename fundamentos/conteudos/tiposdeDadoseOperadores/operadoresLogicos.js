4cf343ca7c26b37696ba698b9fad874059e2df88
pedro falcao <maximosjoselages@gmail.com>
2022-07-18 14:17:12 -0300

    rs

--- fundamentos/conteudos/tiposdeDadoseOperadores/operadoresLogicos.js
+++ fundamentos/conteudos/tiposdeDadoseOperadores/operadoresLogicos.js
@@ -5,8 +5,35 @@
                 or (||)-- "comando logico ou"
                 not (!) -- "negação logica"
               }
+
               respostas dos operadores logicos {
                 sim (true)
                 falso (false)
+              }
+
+              tabela verdar {
+              (and) &&{
+              Condição 1    Condição 2   Resultado
+              Verdade       Verdade        Verdade
+              Verdade         Falso         Falso
+              Falso           Verdade       Falso
+              Falso           Falso         Falso
+              }
+
+              (or) ||{
+              Condição 1 |  Condição 2    Resultado
+              Verdade       Verdade        Verdade
+              Verdade        Falso         Verdade
+              Falso          Verdade        Verdade
+              Falso           Falso          Falso
+              }
+              
+              (xor) ||{
+              Condição 1   Condição 2    Resultado
+              false        │ false      │ false │
+              false        │ true       │ true  │
+              true         │ false      │ true  │
+              true         │ true       │ false
+              }
               }
 */
